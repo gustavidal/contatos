70861eff17ff4b0bbbf4ffbf1597328d0f11e20b
GUSTAVO VIDAL DE ABREU <25203686@dsn127m1246731.cfp127aluno.local>
2025-12-01 16:57:09 -0300

    Código completo

--- src/br/com/gustavo/contatos/ContatosApp.java
+++ src/br/com/gustavo/contatos/ContatosApp.java
@@ -7,19 +7,19 @@ import java.util.Scanner;
 
 public class ContatosApp {
     public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//
-//        Contato c1 = new Contato();
-//        System.out.print("Digite o nome do contato: ");
-//        c1.nome = sc.nextLine();
-//        System.out.print("Digite o email do contato: ");
-//        c1.email = sc.nextLine();
-//        System.out.print("Digite o telefone do contato: ");
-//        c1.telefone = sc.nextLine();
+        Scanner sc = new Scanner(System.in);
+
+        Contato c1 = new Contato();
+        System.out.print("Digite o nome do contato: ");
+        c1.nome = sc.nextLine();
+        System.out.print("Digite o email do contato: ");
+        c1.email = sc.nextLine();
+        System.out.print("Digite o telefone do contato: ");
+        c1.telefone = sc.nextLine();
 
         ContatoRepository cr = new ContatoRepository();
+        cr.contato = c1;
+        cr.gravar();
         cr.listarContatos();
-//        cr.contato = c1;
-//        cr.gravar();
     }
 }
--- src/br/com/gustavo/contatos/repository/ContatoRepository.java
+++ src/br/com/gustavo/contatos/repository/ContatoRepository.java
@@ -41,8 +41,5 @@ public class ContatoRepository {
         } catch (IOException erro) {
             erro.printStackTrace();
         }
-
-
     }
-
 }
